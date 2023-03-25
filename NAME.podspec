c7cdae5a2495c4c529d3e3e6325bd92b9f5666fa
felikslv <felikslv@163.com>
2023-03-25 17:46:47 +0800

    first commit

---
+++ NAME.podspec
@@ -0,0 +1,23 @@
+Pod::Spec.new do |s|
+  s.name             = '${POD_NAME}'
+  s.version          = '0.0.1'
+  s.summary          = 'A short description of ${POD_NAME}.'
+  s.homepage         = 'https://github.com/${USER_NAME}/${POD_NAME}'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { '${USER_NAME}' => '${USER_EMAIL}' }
+  s.source           = { :git => 'https://github.com/${USER_NAME}/${POD_NAME}.git', :tag => s.version.to_s }
+
+  s.ios.deployment_target = '11.0'
+  s.swift_version = '5.0'
+  
+  s.prefix_header_file = false
+  s.source_files = 'Sources/**/*.{swift,h,m,mm}'
+
+    # s.resource_bundles = {
+  #   '${POD_NAME}' => ['${POD_NAME}/Assets/*.png']
+  # }
+
+  # s.public_header_files = 'Pod/Classes/**/*.h'
+  # s.frameworks = 'UIKit'
+  # s.dependency 'SnapKit'
+end
---
+++ setup/ConfigureSwift.rb
@@ -0,0 +1,34 @@
+module Pod
+
+  class ConfigureSwift
+    attr_reader :configurator
+
+    def self.perform(options)
+      new(options).perform
+    end
+
+    def initialize(options)
+      @configurator = options.fetch(:configurator)
+    end
+
+    def perform
+      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
+      configurator.set_test_framework "xctest", "swift", "swift"
+
+      Pod::ProjectManipulator.new({
+        :configurator => @configurator,
+        :xcodeproj_path => "templates/Example/PROJECT.xcodeproj",
+        :platform => :ios,
+        :remove_demo_project => (keep_demo == :no),
+        :prefix => ""
+      }).run
+
+      # There has to be a single file in the Classes dir
+      # or a framework won't be created
+      `touch Pod/Sources/ReplaceMe.swift`
+
+      `mv ./templates/* ./`
+    end
+  end
+
+end
